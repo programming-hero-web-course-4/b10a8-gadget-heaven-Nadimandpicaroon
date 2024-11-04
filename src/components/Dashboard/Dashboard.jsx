12a3da25782664203225fd06c686f57b8b19131f
Nadimandpicaroon <hasamahmudnadim5656@gmail.com>
2024-11-04 17:30:48 +0600

    added some button

---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div>
+      h2 <div className="text-5xl">This is dashboard</div>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-5xl">This is home page</h2>
+    </div>
+  );
+};
+
+export default Home;
